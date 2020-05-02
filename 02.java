7590ea01ce1d82c4ba8f8edf7d7f8f17479ef7f7
amulya12 <30744874+amulya12@users.noreply.github.com>
2020-05-02 18:10:43 +0530

    Jewels and Stones

--- 02.java
+++ 02.java
@@ -1,10 +1,13 @@
+//Approach : with the use of a map I count the frequency of each jewel and add the total frequency.
 class Solution {
     public int numJewelsInStones(String J, String S) {
         Map<Character,Integer> m=new HashMap<Character,Integer>();
+        // first I read each character in string S and store it in map as keys and theirs values are occurances.
         for(char c : S.toCharArray()){
             m.put(c, m.getOrDefault(c, 0) + 1);
         }
         int count = 0;
+        //now i read the characters of jeweles, if a character in jewels is present in map, then I add the value to my tootal count.
         for(char c : J.toCharArray()){
             if(m.containsKey(c)){
                 count = count + m.get(c);
